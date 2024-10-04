217eac70d8987053c61d2c9c5f87aef3dd0ad997
lamlaika rayane <139387044+raynlamlaika@users.noreply.github.com>
2024-10-04 13:10:15 +0100

    Update libft.h

--- libft functions/libft.h
+++ libft functions/libft.h
@@ -44,6 +44,10 @@ void ft_putstr_fd(const char *s, int fd);
 void ft_putnbr_fd(int n, int fd	);
 t_list	*ft_lstnew(void *content);
 char    *ft_strnstr(const char *s, const char *find, size_t slen);
+void ft_lstadd_back(t_list **lst, t_list *new);
+t_list *ft_lstlast(t_list *lst);
+int ft_lstsize(t_list *lst);
+void ft_putendl_fd(char *s, int fd);
 
 
 #endif
